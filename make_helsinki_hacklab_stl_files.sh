2ba12f1fd6dc1bf9eb49f40acf93825a9eb06139
zzorn <zzorn@iki.fi>
2015-05-18 19:50:12 +0300

    Updated config and creation script.

--- make_helsinki_hacklab_stl_files.sh
+++ make_helsinki_hacklab_stl_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Builds the stl files for the Mendel90 configuration used by helsinki hacklab.
-# Make sure you have python and openscad on the path.
+# Make sure you have python, openscad and inkscape on the path!
 
-make_machine.py helsinki_hacklab
+./make_machine.py helsinki_hacklab
 
 # Afterwards, to view the model of the whole machine, open scad\main.scad. 
 # It will take several minutes to render but after that you can pan and zoom it at reasonable speed and changes takes less time to render.
