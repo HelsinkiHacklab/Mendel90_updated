baae3dbf4f586ced426400154bb633b028803204
zzorn <zzorn@iki.fi>
2015-05-18 19:13:54 +0300

    Added script to generate the stl files for the Helsinki Hacklab variant.

---
+++ make_helsinki_hacklab_stl_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Builds the stl files for the Mendel90 configuration used by helsinki hacklab.
+# Make sure you have python and openscad on the path.
+
+make_machine.py helsinki_hacklab
+
+# Afterwards, to view the model of the whole machine, open scad\main.scad. 
+# It will take several minutes to render but after that you can pan and zoom it at reasonable speed and changes takes less time to render.
+# To view a sub-assembly, open the individual scad files. Set the exploded flag in config.scad to true to make exploded views (and re-run this script).
+
+
